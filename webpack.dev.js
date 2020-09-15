d610ff50670429b4d9c35ffaf1799cefed443b28
Toby Jorris <tobyjorris@gmail.com>
2020-09-15 09:14:37 -0500

    Bundle Analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
-const path = require('path');
-const webpack = require('webpack');
+const { path } = require('path');
+const { webpack } = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,6 +21,13 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
-        })
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false,
+        }),
+        new BundleAnalyzerPlugin(),
     ]
 }
